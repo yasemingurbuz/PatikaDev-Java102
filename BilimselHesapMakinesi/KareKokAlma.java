9f0a471e0adf34f34788aac11341296593878ae0
yasemingurbuz <84576138+yasemingurbuz@users.noreply.github.com>
2021-08-31 21:41:39 +0300

    Add files via upload

---
+++ BilimselHesapMakinesi/KareKokAlma.java
@@ -0,0 +1,21 @@
+package BilimselHesapMakinesi;
+
+import java.util.Scanner;
+
+public class KareKokAlma {
+    static void kareKokAlma() {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Hangi sayının karekökünü almak istersiniz?: ");
+        double n = input.nextDouble();
+
+        double result = Math.sqrt(n);
+
+        if (n < 0) {
+            System.out.println("Girilen sayı pozitif olmalıdır!");
+        }
+        else {
+            System.out.println(n + " sayısının karekökü: " + result);
+        }
+    }
+}
